fab17e27c8bed51b69a81c5c80d08d6744218a06
martinliu <liuzh66@gmail.com>
2020-08-27 16:18:39 +0800

    upate readme

--- README.md
+++ README.md
@@ -1,16 +1,27 @@
-# elasticstack
+# Elastic Stack 实验室
+
+相关文章
+
+- https://martinliu.cn/posts/elasticsearch-3-nodes-cluster-setup/
+
+
+
+## 贡献
+
+请帮忙优化这里的配置和脚本。
 
     es1: Changed password for user apm_system
-    es1: PASSWORD apm_system = wtserSjxdbRwvjAhIaQq
+    es1: PASSWORD apm_system = Aqz7rsQipjrFaayFkueP
     es1: Changed password for user kibana_system
-    es1: PASSWORD kibana_system = Ils2feList7EugGpz3jB
+    es1: PASSWORD kibana_system = 8Ftd3UTWTgkK0Jwd84EK
     es1: Changed password for user kibana
-    es1: PASSWORD kibana = Ils2feList7EugGpz3jB
+    es1: PASSWORD kibana = 8Ftd3UTWTgkK0Jwd84EK
     es1: Changed password for user logstash_system
-    es1: PASSWORD logstash_system = D4xCM2DM9FrMXGnNEMtx
+    es1: PASSWORD logstash_system = wYv5ylBthLtJQUJKj4HK
     es1: Changed password for user beats_system
-    es1: PASSWORD beats_system = FAb6EhVJHL8aGoHyQFxy
+    es1: PASSWORD beats_system = YGIAZvtmUL9N7TdHOodL
     es1: Changed password for user remote_monitoring_user
-    es1: PASSWORD remote_monitoring_user = 68lgMvlfXhqx6N2m692p
+    es1: PASSWORD remote_monitoring_user = 1Z44yF92wcHvI78DsKUv
     es1: Changed password for user elastic
-    es1: PASSWORD elastic = 3J0QRZEUFlyTu8sHGouZ
+    es1: PASSWORD elastic = g8sIdxm0B9D7TFzsBX4f
+    es1: Provisioning script works good!
---
+++ rpm/get-rpm.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# author: Martin Liu
+# url:martinliu.cn
+# 下载所需要的 rpm 安装包 
+
+elastic_version='7.9.0'
+b_rpm='https://mirrors.cloud.tencent.com/elasticstack/7.x/yum'
+
+wget https://mirrors.cloud.tencent.com/elasticstack/7.x/yum/7.9.0/apm-server-7.9.0-x86_64.rpm
+
+wget https://mirrors.cloud.tencent.com/elasticstack/7.x/yum/7.9.0/auditbeat-7.9.0-x86_64.rpm
+
+wget https://mirrors.cloud.tencent.com/elasticstack/7.x/yum/7.9.0/elastic-agent-7.9.0-x86_64.rpm
+
+wget https://mirrors.cloud.tencent.com/elasticstack/7.x/yum/7.9.0/elasticsearch-7.9.0-x86_64.rpm
+
+wget https://mirrors.cloud.tencent.com/elasticstack/7.x/yum/7.9.0/enterprise-search-7.9.0.rpm
+
+wget https://mirrors.cloud.tencent.com/elasticstack/7.x/yum/7.9.0/filebeat-7.9.0-x86_64.rpm
+
+wget https://mirrors.cloud.tencent.com/elasticstack/7.x/yum/7.9.0/heartbeat-7.9.0-x86_64.rpm
+
+wget https://mirrors.cloud.tencent.com/elasticstack/7.x/yum/7.9.0/journalbeat-7.9.0-x86_64.rpm
+
+wget https://mirrors.cloud.tencent.com/elasticstack/7.x/yum/7.9.0/kibana-7.9.0-x86_64.rpm
+
+wget https://mirrors.cloud.tencent.com/elasticstack/7.x/yum/7.9.0/logstash-7.9.0.rpm
+
+wget https://mirrors.cloud.tencent.com/elasticstack/7.x/yum/7.9.0/metricbeat-7.9.0-x86_64.rpm
+
+wget https://mirrors.cloud.tencent.com/elasticstack/7.x/yum/7.9.0/packetbeat-7.9.0-x86_64.rpm
